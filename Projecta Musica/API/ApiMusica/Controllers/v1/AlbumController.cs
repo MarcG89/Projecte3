4524f84842f140209d507396c867664619b6af12
Youssef-ai-svg <yfdil690@boscdelacoma.cat>
2024-02-22 21:35:31 +0100

    Controller i Service d'Album fets, no fet el test nomes el metode

--- Projecta Musica/API/ApiMusica/Controllers/v1/AlbumController.cs
+++ Projecta Musica/API/ApiMusica/Controllers/v1/AlbumController.cs
@@ -1,6 +1,77 @@
-﻿namespace ApiMusica.Controllers.v1
+﻿using ApiMusica.Classes.Model;
+using ApiMusica.Controllers.v1.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace ApiMusica.Controllers.v1
 {
-    public class AlbumController
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AlbumController : ControllerBase
     {
+        private readonly AlbumService _albumService;
+        private readonly ILogger<AlbumController> _logger;
+
+        public AlbumController(AlbumService albumService, ILogger<AlbumController> logger)
+        {
+            _albumService = albumService;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Album>> Get() =>
+            _albumService.Get();
+
+        [HttpGet("{id:length(24)}", Name = "GetAlbum")]
+        public ActionResult<Album> Get(string id)
+        {
+            var album = _albumService.Get(id);
+
+            if (album == null)
+            {
+                return NotFound();
+            }
+
+            return album;
+        }
+
+        [HttpPost]
+        public ActionResult<Album> Create(Album album)
+        {
+            _albumService.Create(album);
+
+            return CreatedAtRoute("GetAlbum", new { id = album.Titol.ToString() }, album);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Album albumIn)
+        {
+            var album = _albumService.Get(id);
+
+            if (album == null)
+            {
+                return NotFound();
+            }
+
+            _albumService.Update(id, albumIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var album = _albumService.Get(id);
+
+            if (album == null)
+            {
+                return NotFound();
+            }
+
+            _albumService.Remove(album.Titol);
+
+            return NoContent();
+        }
     }
 }
--- Projecta Musica/API/ApiMusica/Controllers/v1/Services/AlbumService.cs
+++ Projecta Musica/API/ApiMusica/Controllers/v1/Services/AlbumService.cs
@@ -1,6 +1,35 @@
-﻿namespace ApiMusica.Controllers.v1.Services
+﻿using System.Collections.Generic;
+using ApiMusica.Classes.Model;
+using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
+
+namespace ApiMusica.Controllers.v1.Services
 {
     public class AlbumService
     {
+        private readonly IMongoCollection<Album> _albums;
+
+        public AlbumService(IMongoDatabase database)
+        {
+            _albums = database.GetCollection<Album>("albums");
+        }
+
+        public List<Album> Get() =>
+            _albums.Find(album => true).ToList();
+
+        public Album Get(string id) =>
+            _albums.Find<Album>(album => album.Titol == id).FirstOrDefault();
+
+        public Album Create(Album album)
+        {
+            _albums.InsertOne(album);
+            return album;
+        }
+        public void Update(string id, Album albumIn) =>
+            _albums.ReplaceOne(album => album.Titol == id, albumIn);
+        public void Remove(Album albumIn) =>
+            _albums.DeleteOne(album => album.Titol == albumIn.Titol);
+        public void Remove(string id) =>
+            _albums.DeleteOne(album => album.Titol == id);
     }
 }
