02092a5b0c9cefce3a5dba865888b53dfbbfe165
Youssef-ai-svg <yfdil690@boscdelacoma.cat>
2024-02-29 13:23:33 +0100

    refactor del codi d'Album per cuadrar amb el android (youssef / marc)

--- Projecta Musica/API/ApiMusica/Classes/Model/Album.cs
+++ Projecta Musica/API/ApiMusica/Classes/Model/Album.cs
@@ -12,7 +12,7 @@ namespace ApiMusica.Classes.Model
         public string Name { get; set; }
         public int Year { get; set; }
         public string Gender { get; set; }
-     
+
         public string FrontCover { get; set; }
         public string BackCover { get; set; }
 
