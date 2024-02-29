a5222f2ce19660c34506564786979e8445443c74
Youssef-ai-svg <yfdil690@boscdelacoma.cat>
2024-02-29 13:29:28 +0100

    backup

--- Projecta Musica/API/ApiMusica/obj/Debug/net8.0/ApiMusica.AssemblyInfo.cs
+++ Projecta Musica/API/ApiMusica/obj/Debug/net8.0/ApiMusica.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiMusica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9b1b0b56398e832ff9a091bdb8420abec664774")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiMusica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiMusica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
